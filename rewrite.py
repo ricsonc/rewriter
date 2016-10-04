c2db6b8f2ad475c472e272e36106ef507f2422c9
Ricson Cheng <ricsonc@andrew.cmu.edu>
2016-10-04 18:47:24 -0400

    added code to check for possible typoes

--- rewrite.py
+++ rewrite.py
@@ -1,9 +1,13 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 import sys
 
 def rewrite(in_string, parameters):
     for param in parameters:
+        if '\\'+param+'\\' not in in_string:
+            print "WARNING : parameter "+param+" not used."
         in_string = in_string.replace('\\'+param+'\\', parameters[param])
+    if '\\' in in_string:
+        print 'WARNING : string may not be fully specified'
     return in_string
 
 def parse(paramstring):
